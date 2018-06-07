0570846349c0281d3fee4e8fa7d73f4f733a500e
Karthick1808 <karthickspikey@gmail.com>
2018-06-07 21:51:47 +0530

    Add files via upload

---
+++ Java.sh
@@ -0,0 +1,6 @@
+add-apt-repository ppa:webupd8team/java
+apt-get update -y
+apt-get install oracle-java8-installer -y
+echo 'JAVA_HOME="/usr/lib/jvm/java-8-oracle"' >> /etc/environment
+source /etc/environment
+echo $JAVA_HOME
