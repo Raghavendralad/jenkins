bafd5b67e480e3b3fc639c30e4a5fc584b4092ed
Karthick1808 <karthickspikey@gmail.com>
2018-06-09 16:53:28 +0530

    Update Java.sh

--- Java.sh
+++ Java.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-add-apt-repository ppa:webupd8team/java
+add-apt-repository ppa:webupd8team/java -y
 apt-get update -y
 apt-get install oracle-java8-installer -y
 echo 'JAVA_HOME="/usr/lib/jvm/java-8-oracle"' >> /etc/environment
